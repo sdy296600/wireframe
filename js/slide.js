23c1af54346c0488ddb4e46d0c715ace72890340
sdy29600 <sdy29600@github.com>
2025-08-26 18:10:31 +0900

    .

--- js/slide.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", function () {
-  //
-  //   const viewSlide = this.document.querySelector("#.slide-wrap");
-  //   const slideImg = this.document.querySelector("#.slide");
-  //   viewSlide.forEach((item, index) => {
-  //     const currentIndex = slideImg[index];
-  //     console.log(currentIndex);
-  //   });
-});
